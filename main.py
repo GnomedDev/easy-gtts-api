da4132e90b9a1556655f681445e2a359de42d6fa
Gnome <david2005thomas@gmail.com>
2021-04-07 19:38:29 +0100

    Add /v1/tts, which passes gTTS exceptions through to user

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 
 import fastapi
-from gtts import gTTS
+from gtts import gTTS, gTTSError
 from gtts.lang import tts_langs
 
 app = fastapi.FastAPI()
@@ -21,6 +21,28 @@ def tts(text, lang="en-us"):
     mp3.seek(0)
     return fastapi.responses.StreamingResponse(mp3, media_type="audio/mp3")
 
+@app.get("/v1/tts")
+def v1_tts(text, lang="en-us"):
+    if lang not in tts_langs:
+        raise fastapi.HTTPException(status_code=400, detail=f"{lang} is not a valid language code.")
+
+    mp3 = BytesIO()
+    try:
+        gTTS(text=text, lang=lang).write_to_fp(mp3)
+    except gTTSError as e:
+        if e.rsp:
+            raise fastapi.HTTPException(
+                status_code=e.rsp.status_code,
+                detail=e.rsp.content,
+                headers=e.rsp.headers
+                )
+
+        raise
+
+    mp3.seek(0)
+    return fastapi.responses.StreamingResponse(mp3, media_type="audio/mp3")
+
 @app.get("/langs")
+@app.get("/v1/langs")
 async def get_langs():
     return tts_langs
