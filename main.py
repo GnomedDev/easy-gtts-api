0ee2c5c934411ce1b481c210938dddf5eb6a6b3c
Gnome <david2005thomas@gmail.com>
2021-04-12 11:25:43 +0100

    Clean up code, and add POST /v1/tts

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+from functools import wraps
 from io import BytesIO
 
-import fastapi
+from fastapi import Body, FastAPI, HTTPException
+from fastapi.responses import RedirectResponse, StreamingResponse
 from gtts import gTTS, gTTSError
 from gtts.lang import tts_langs
 
-app = fastapi.FastAPI()
+app = FastAPI()
 tts_langs = tts_langs()
+
+def verify_lang(func):
+    @wraps(func)
+    def replace_func(text, lang="en"):
+        if lang not in tts_langs:
+            raise HTTPException(
+                detail=f"{lang} is not a valid language code.",
+                status_code=400
+                )
+
+        return func(text, lang)
+
+    return replace_func
 
 @app.get("/")
 async def root():
-    return fastapi.responses.RedirectResponse("/docs")
+    return RedirectResponse("/docs")
 
+@verify_lang
 @app.get("/tts")
-def tts(text, lang="en-us"):
-    if lang not in tts_langs:
-        raise fastapi.HTTPException(status_code=400, detail=f"{lang} is not a valid language code.")
-
+def tts(text, lang="en"):
     mp3 = BytesIO()
     gTTS(text=text, lang=lang).write_to_fp(mp3)
     mp3.seek(0)
-    return fastapi.responses.StreamingResponse(mp3, media_type="audio/mp3")
 
+    return StreamingResponse(mp3, media_type="audio/mp3")
+
+@verify_lang
 @app.get("/v1/tts")
-def v1_tts(text, lang="en-us"):
-    if lang not in tts_langs:
-        raise fastapi.HTTPException(status_code=400, detail=f"{lang} is not a valid language code.")
-
+def v1_tts(text, lang="en"):
     mp3 = BytesIO()
     try:
         gTTS(text=text, lang=lang).write_to_fp(mp3)
     except (AssertionError, ValueError, RuntimeError) as e:
-        raise fastapi.HTTPException(
+        raise HTTPException(
             status_code=400,
             detail=str(e)
         )
@@ -40,7 +52,7 @@ def v1_tts(text, lang="en-us"):
             headers = e.rsp.headers
             headers.pop("Content-Length", None)
 
-            raise fastapi.HTTPException(
+            raise HTTPException(
                 status_code=e.rsp.status_code,
                 detail=e.rsp.content.decode(),
                 headers=headers
@@ -49,7 +61,11 @@ def v1_tts(text, lang="en-us"):
         raise
 
     mp3.seek(0)
-    return fastapi.responses.StreamingResponse(mp3, media_type="audio/mp3")
+    return StreamingResponse(mp3, media_type="audio/mp3")
+
+@app.post("/v1/tts")
+def v1_tts_post(text: str = Body(...), lang: str = Body(default="en")):
+    return v1_tts(text, lang)
 
 @app.get("/langs")
 @app.get("/v1/langs")
